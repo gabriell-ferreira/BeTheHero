6e5e0601549ea7852d8128cef14b4250412eeb58
gabriell-ferreira <araujogabriel80@hotmail.com>
2021-10-21 22:58:22 -0400

    Create button component

---
+++ src/components/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <input 
+    id="buttonComponent"
+    type="button" 
+    :value="value" 
+    @click="$emit('click')"
+    :style="'width:'+width"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    width: {
+      type: String,
+      default: '100%'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #buttonComponent {
+    height: 3.75rem;
+    
+    border-radius: 8px;
+    border: 0;
+
+    font-weight: 700;
+    font-size: 1.125rem;
+
+    background-color: var(--red);
+    color: var(--inputs);
+    cursor: pointer;
+  }
+</style>
